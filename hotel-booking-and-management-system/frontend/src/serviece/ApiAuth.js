5de9b6a04cef7513f428f2f57e315586c33dfebc
Nghia <nguyenphuocnghia@gmail.com>
2025-12-12 13:21:36 +0700

    fix: update ApiAuth logic in frontend

--- hotel-booking-and-management-system/frontend/src/serviece/ApiAuth.js
+++ hotel-booking-and-management-system/frontend/src/serviece/ApiAuth.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE_URL = typeof window !== 'undefined' && window.location.hostname === 'localhost' ? '' : 'http://160.191.245.177:8000';
+//const API_BASE_URL = typeof window !== 'undefined' && window.location.hostname === 'localhost' ? '' : 'http://160.191.245.177:8000';
+const API_BASE_URL = '';
 
 // Tạo instance axios
 const apiClient = axios.create({
